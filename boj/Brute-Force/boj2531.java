0c6362056e10dd8c50206ab8ec71ccbdbbd26075
Dagon <leedagon624@gmail.com>
2024-01-09 08:57:33 +0900

    sliding window 문제 boj2531 추가

---
+++ boj/Brute-Force/boj2531.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj2531 {
+    
+    static int n; //접시 수
+    static int d; // 초밥 가짓수
+    static int k; // 연속 먹는 접시 수
+    static int c; // 쿠폰 번호
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        int ans = 0;
+        int[] rail = new int[n];
+        for(int i = 0; i < n; i++){
+            rail[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = 0; i < n; i++){
+            int[] check = new int[d + 1];
+            int cnt = 0;
+            boolean bonus_included = false;
+            for(int j = i; j < i + k; j++){
+                check[rail[j % n]] ++;
+                if(rail[j % n] == c){
+                    bonus_included = true;
+                }
+            }
+            if(!bonus_included){
+                cnt++;
+            }
+            for(int j = 1; j <= d; j++){
+                if(check[j] > 0){
+                    cnt++;
+                }
+            }
+            ans = Math.max(ans, cnt);
+        }
+        System.out.println(ans);
+    }
+
+}
