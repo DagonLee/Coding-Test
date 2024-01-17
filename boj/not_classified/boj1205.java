cc21ecf5b3849a3393c942b8d045df7f9d2820a0
Dagon <leedagon624@gmail.com>
2024-01-17 16:37:04 +0900

    boj : impl : 1205 : 로직 리팩토링

--- boj/not_classified/boj1205.java
+++ boj/not_classified/boj1205.java
@@ -24,33 +24,17 @@ public class boj1205 {
         for(int i = 0; i < n; i++){
             nums.add(Integer.parseInt(st.nextToken()));
         }
-        int dest = -1;
-        for(int i = 0; i < nums.size(); i++){
+        if(n == p && nums.get(nums.size() - 1) >= score){
+            System.out.println(-1);
+            return;
+        }
+        int dest = n + 1;
+        for(int i = 0; i < n; i++){
             if(nums.get(i) <= score){
-                dest = i;
-                while(dest + 1 < nums.size() && nums.get(dest + 1) == score){
-                    dest ++;
-                }
-                if(dest + 1 == p && nums.get(dest) == score){
-                    System.out.println(-1);
-                    return;
-                }
-                break;
+                System.out.println(i + 1);
+                return;
             }
         }
-        if(dest == -1){
-            if(nums.size() < p ){
-                System.out.println(nums.size() + 1);
-            }
-            else{
-                System.out.println(-1);
-            }
-        }
-        else{
-            while(dest - 1 >= 0 && nums.get(dest - 1) == score){
-                dest--;
-            }
-            System.out.println(dest + 1);
-        }
+        System.out.println(dest);
     }
 }
